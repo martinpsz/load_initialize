b2b150ae08233a3a50ffcd5a2d5d26421eec3960
Martin Pszczola <martinpsz@protonmail.com>
2023-10-26 22:38:26 -0400

    Adding functionality to process large datasets for initial load

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#Imports
 from dotenv import dotenv_values
 import boto3
-import pandas as pd
 import re
-from typing import Literal, Optional
 import json
-import random
+import pandas as pd
+import tempfile
+import gzip
+import csv
+
 
 #Connect to AWS S3 bucket with a new boto session
 config = dict(dotenv_values(".env"))
@@ -19,122 +20,95 @@ session = boto3.session.Session(
     profile_name='afscme-development'
 )
 
-#Constants:
-ARCHIVE = 'event-distribution-s3-archive-testing-b1e0c31'
-PROD = 'event-distrib-dev-acct-test-archive-b0842d1'
-DEST = 'updates-member-card'
+#Connect to and create local file list that matches 'enterprise'
+#Uncomment and run if you don't have a local file with the bucket list already.
+SOURCE_DIR = 'event-distrib-dev-acct-test-archive-b0842d1'
 
-#set up connections to all buckets needed.
 s3_client = session.resource('s3')
-archive_bucket = s3_client.Bucket(f'{ARCHIVE}')
-production_bucket = s3_client.Bucket(f'{PROD}')
-destination_bucket = s3_client.Bucket(f'{DEST}')
+source_bucket = s3_client.Bucket(SOURCE_DIR)
 
-#Regex to filter out events of interest
-PERSON_MEMBER_REGEX = re.compile(r"(person_member(?=_))")
-PERSON_REGEX = re.compile(r"(enterprise_person(?=_\d))")
-ADDRESS_REGEX = re.compile(r"(enterprise_person(?=_address))")
-AFF_MEMBER_REGEX = re.compile(r"(enterprise_aff(?=_member))")
-AFF_ORG_REGEX = re.compile(r"(enterprise_aff(?=_organization))")
+'''
+file_type_regex = re.compile(r'enterprise_(.*)(?=_\d)')
 
+enterprise_event_links = {}
+event_types = []
+for items in source_bucket.objects.filter(Prefix='prod-data/load-2023-10-13/'):
+    regexMatch = re.search(file_type_regex, items.key)
 
-#Generate lists of event files that match the event of interest
-event_dict = {'PERSON_MEMBER': PERSON_MEMBER_REGEX,
-              'PERSON': PERSON_REGEX,
-              'ADDRESS': ADDRESS_REGEX,
-              'AFF_MEMBER': AFF_MEMBER_REGEX,
-              'AFF_ORG': AFF_ORG_REGEX}
+    if regexMatch:
+        event_type = regexMatch.group()
 
-#Retrieve a list of bucket file paths mapped to the event of interest. 
-def event_file_list(BucketPath: Literal['archive_bucket', 'production_bucket', 'destination_bucket'], 
-                    BucketPrefix: Optional[str] = None, eventRegex=event_dict) -> any:
+        if event_type not in enterprise_event_links:
+            enterprise_event_links[event_type] = []
+
+        enterprise_event_links[event_type].append(items.key)
     
-    d = {}
-    for key, value in eventRegex.items():
-      l = []
-      for item in BucketPath.objects.filter(Prefix=BucketPrefix):
-        regexMatch = re.search(value, item.key)
-
-        if regexMatch:
-           l.append(item.key)
-
-      d[key] = l
-
-    return d
-
-
-'''
-  To generate a list of file paths for each event of interest, run create_local_event_file_list below and
-  you'll get a local copy.
+json_object = json.dumps(enterprise_event_links, indent=4)
+with open('prod_data_links.json', 'w') as f:
+    f.write(json_object)
 '''
 
-def create_local_event_file_list(fileName: str) -> None:
-  events = json.dumps(event_file_list(archive_bucket, 'prod-initial-load'))
+with open('prod_data_links.json') as f:
+    event_links = json.load(f)
 
 
-  with open(f"{fileName}.json", 'w') as file:
-    file.write(events)
-
-
-
-#Open local copy of events list created and process for initial load or test file generation:
-with open('event_list.json') as e:
-   event_paths = json.load(e)
-
-
-#Let's generate a test file first:
-test_dict = {}
-for key, values in event_paths.items():
-   test_dict[key] = random.choice(values)
-   
-
-with open('test.jsonl.gz', 'wb') as data:
-   archive_bucket.download_fileobj(test_dict['PERSON_MEMBER'], data)
-
-  
-   
-
-   # 2. If a valid link, attempt to unzip and read it in with pandas json
-
-   # 3. Check if file has dupe data...log file name and number of non-unique key values. Sort by ts, take latest value if dupes. 
-
-   # 4. 
+#for k, v in event_links.items():
+#    print(k, v[0])
 
 
 
 
+def retrieveAndSaveHeaders(bucket, file, event_name):
+
+    with tempfile.NamedTemporaryFile() as f: 
+        bucket.download_fileobj(file, f)
+
+        cols = []
+        with gzip.open(f.name, 'rb') as g:
+            df = pd.read_json(g, lines=True)
+            data = pd.json_normalize(df['data'])
+
+            cols.append(data.columns.to_list())
+            cols[0].append('ts')
+
+            with open(f"{event_name}_headers.tsv", 'w') as h:
+                writer = csv.writer(h, delimiter='\t')
+
+                for col in cols:
+                    writer.writerow(col)
+
+        f.close()
+
+#for k, v in event_links.items():
+#    retrieveAndSaveHeaders(source_bucket, v[0], k)
 
 
+def returnUniqueAndLatestEventData():
+    test_file = event_links['enterprise_aff_organization'][0]
+
+    hashMap = {}
+    with tempfile.NamedTemporaryFile() as f:
+        source_bucket.download_fileobj(test_file, f)
+
+        with gzip.open(f.name, 'rb') as g:
+            df = pd.read_json(g, lines=True)
+            data = pd.json_normalize(df['data'])
+
+            timestamp = df['ts']
+            primary_key = data['aff_pk'] 
+
+            
+
+            #if primary_key in hashMap:
+            #    if timestamp > hashMap[primary_key][1]:
+            #        hashMap[primary_key] = (data, timestamp)
+            #else:
+            #    hashMap[primary_key] = (data, timestamp)
+    
+    #print(hashMap)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-#Connect to AWS S3 bucket with initial data partitions
-
-
-
-#Loop through each partition and retrieve but don't store zip file (leave in memory)
-
-#Put each files data in one temp file bc you'll need to check for duplicates across all partitions
-
-#Sort by ts and keep latest duplicated value if existent.
-
-#Load files from prod_file...follow same steps as above to get up to date, non-repeating data.
-
-#Compare initial and prod files, update rows with prod if prod is not in initial or has update.
-
-#Based on row numbers, cut data into chunks to allow for parallel load on s3 bucket update.
-
-#Connect to s3 bucket and update each tables folder with initialized data.
+returnUniqueAndLatestEventData()
+#Get list of headings and save to file for Viktoriya
+#Deduplication process
+#Generate tsv files
